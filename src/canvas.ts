93df5861aa850c9055481c44f3c4a6d964397a85
joao-arthur <joao.lothamer@gmail.com>
2022-09-15 11:22:24 -0300

    chore: add canvas handler

---
+++ src/canvas.ts
@@ -0,0 +1,12 @@
+export function initCanvasPaint(
+    context: CanvasRenderingContext2D,
+    dimensions: any,
+) {
+    context.fillStyle = '#5555aa';
+    context.fillRect(
+        100,
+        100,
+        100,
+        100,
+    );
+}
