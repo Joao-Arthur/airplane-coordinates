c3c132d8c4605dc8fb791f8b194f83e8880c88ac
joao-arthur <joao.lothamer@gmail.com>
2022-09-19 19:48:00 -0300

    fix: number fns order of appy functions

--- src/core/numberFns/fix/fix.ts
+++ src/core/numberFns/fix/fix.ts
@@ -4,7 +4,7 @@ import { toScale } from "../toScale";
 
 export function fix(initialValue: number) {
     return pipe(
+        value => toScale(value, 14),
         value => normalizeZero(value),
-        value => toScale(value, 14)
     )(initialValue);
 }
