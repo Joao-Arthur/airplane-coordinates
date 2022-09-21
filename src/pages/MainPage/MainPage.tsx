f07ea4ec4f3a0a6f32dc8fa2c5e87c79da02ee5d
joao-arthur <joao.lothamer@gmail.com>
2022-09-20 21:35:15 -0300

    refactor: main page to main layout

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,55 +1,10 @@
-import { Tab } from "../../components/Tab";
-import { AddAirplane } from "./AddAirplane";
-import { AirplanesTable } from "./AirplanesTable";
-import { CloseToAirport } from "./CloseToAirport";
-import { CloseToEachOther } from "./CloseToEachOther";
-import { InRouteOfCollision } from "./InRouteOfCollision";
-import { Radar } from "./Radar";
-import { Report } from "./Report";
-import { Rotate } from "./Rotate";
-import { Scalonate } from "./Scalonate";
-import { Translate } from "./Translate";
+import { MainLayout } from "./MainLayout";
+
 
 export function MainPage() {
     return (
         <>
-            <div className="relative w-1/4">
-                <Tab
-                    tabs={[
-                        {
-                            title: 'Adicionar',
-                            comp: <AddAirplane />
-                        },
-                        {
-                            title: 'Transformar',
-                            comp: (
-                                <div>
-                                    <Translate />
-                                    <Scalonate />
-                                    <Rotate />
-                                </div>
-                            )
-                        },
-                        {
-                            title: 'Rastrear',
-                            comp: (
-                                <div>
-                                    <CloseToAirport />
-                                    <CloseToEachOther />
-                                    <InRouteOfCollision />
-                                </div>
-                            )
-                        },
-                    ]}
-                />
-            </div>
-            <div className="relative w-1/4">
-                <AirplanesTable />
-                <Report />
-            </div>
-            <div className="relative w-1/4">
-                <Radar />
-            </div>
+            <MainLayout />
         </>
     )
 }
