169027d4017537d089f7868776cc466c18f53aad
joao-arthur <joao.lothamer@gmail.com>
2022-09-30 10:30:06 -0300

    chore: fix eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,31 +6,25 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:react/recommended',
-        'plugin:jsx-a11y/recommended',
-        'airbnb',
-        'airbnb-typescript',
+        'plugin:@typescript-eslint/recommended'
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
-        project: ['./tsconfig.json'],
-        ecmaFeatures: {
-            jsx: true,
-        },
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
-
-    plugins: ['react', 'react-hooks', 'jsx-a11y', '@typescript-eslint'],
+    plugins: ['react', 'react-hooks', '@typescript-eslint'],
     rules: {
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 'error',
-        'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+        'react/jsx-filename-extension': ['error', {
+            extensions: ['.tsx']
+        }],
         'react/jsx-uses-react': 'off',
         'react/react-in-jsx-scope': 'off',
         'react/jsx-indent': ['error', 4],
         'react/jsx-indent-props': ['error', 4],
         'import/prefer-default-export': 'off',
-        'import/no-default-export': 'error',
         '@typescript-eslint/naming-convention': [
             'error',
             {
@@ -47,20 +41,35 @@ module.exports = {
         'operator-linebreak': [
             'error',
             'after',
-            { overrides: { '?': 'before', ':': 'before' } },
+            {
+                overrides: {
+                    '?': 'before', ':': 'before'
+                }
+            },
         ],
         'no-implicit-globals': 'error',
         'default-case': 'off',
         'no-restricted-syntax': 'off',
         curly: ['error', 'multi'],
         'nonblock-statement-body-position': ['error', 'below'],
-        'semi': ['error', 'always'],
         'object-curly-newline': ['error', {
-            'ObjectExpression': 'multiline',
-            'ObjectPattern': 'multiline',
-            'ImportDeclaration': 'never',
-            'ExportDeclaration': 'multiline'
+            'ObjectExpression': {
+                multiline: true,
+                consistent: true
+            },
+            'ObjectPattern': {
+                'multiline': true
+            },
+            'ImportDeclaration': {
+                multiline: false, consistent: true
+            },
+            'ExportDeclaration': {
+                'multiline': true, 'minProperties': 0
+            }
         }],
-        quotes: ["error", "single"]
-    },
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+    }
 };
