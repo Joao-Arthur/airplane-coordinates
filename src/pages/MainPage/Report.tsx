5f448b0791f0f4c4d3e73169e4b6cc1bc1124334
joao-arthur <joao.lothamer@gmail.com>
2022-10-04 16:28:16 -0300

    feat: report color

--- src/pages/MainPage/Report.tsx
+++ src/pages/MainPage/Report.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import { nanoid } from 'nanoid';
 import { useAirplaneStore } from '../../integrations/airplane/airplanesStore';
 
@@ -10,7 +11,13 @@ export function Report() {
                 <tbody className='divide-y divide-gray-200'>
                     {reports.map(report => (
                         <tr key={nanoid()}>
-                            <td className='px-4 py-2 whitespace-nowrap'>{report.content}</td>
+                            <td className={
+                                classNames('px-4 py-2 whitespace-nowrap',
+                                    report.type === 'success' ? 'bg-green-50' : '',
+                                    report.type === 'error' ? 'bg-red-50' : '',
+                                    report.type === 'warn' ? 'bg-yellow-50' : '',
+                                )
+                            }>{report.content}</td>
                         </tr>
                     ))}
                 </tbody>
