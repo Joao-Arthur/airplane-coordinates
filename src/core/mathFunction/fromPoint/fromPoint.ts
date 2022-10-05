76dfc98d7265e1cdce8af476a6b6ffb1217823df
João Arthur <joao.lothamer@gmail.com>
2022-10-05 20:43:10 -0300

    feat: from point

--- src/core/mathFunction/fromPoint/fromPoint.ts
+++ src/core/mathFunction/fromPoint/fromPoint.ts
@@ -6,10 +6,19 @@ type paramsType = {
     readonly angle: number; 
 };
 
-export function fromPoint({}: paramsType) {
+export function fromPoint({point: { x, y }, angle}: paramsType): cartesianFunctionType {
+    if([90, 270].includes((angle + 360) % 360))
+        return {
+            a: Number.POSITIVE_INFINITY,
+            b: x,
+        };
+    const a = angle * Math.PI / 180;
     
+    const expected = a * x;
+    const b = y = expected;
+
     return {
-        a: ,
-        b: ,
+        a,
+        b,
     };
 }
