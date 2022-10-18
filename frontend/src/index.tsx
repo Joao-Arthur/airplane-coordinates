aa86f0d399a735f2077ea942c1484a7fd2927ea3
joao-arthur <joao.lothamer@gmail.com>
2022-10-18 18:51:51 -0300

    chore: remove backend for temporary time

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,22 +1,6 @@
 import { createRoot } from 'react-dom/client';
-import init, { scalonate } from 'backend';
 import './index.css';
 import { App } from './App';
-
-init().then(() => {
-    console.log(
-        scalonate({
-            point: {
-                x: '1.0',
-                y: '2.0',
-            },
-            factor: {
-                x: '1.111111111111111111111111111111111111111111111111111111111111',
-                y: '1.111111111111111111111111111111111111111111111111111111111111',
-            },
-        }),
-    );
-});
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById('root')!;
