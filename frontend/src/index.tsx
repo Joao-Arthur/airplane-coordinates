b7a51faf2fbac15308cb8dcddc20422abbc51ac7
joao-arthur <joao.lothamer@gmail.com>
2022-10-18 15:31:50 -0300

    chore: setup webassembly integration

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,11 @@
 import { createRoot } from 'react-dom/client';
+import init, { greet } from 'backend';
 import './index.css';
 import { App } from './App';
+
+init().then(() => {
+    greet('WebAssembly');
+});
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById('root')!;
