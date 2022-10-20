aa6a1e4f529db786c9e3729703f5e38d60f507a1
João Arthur <joao.lothamer@gmail.com>
2022-10-20 20:13:01 -0300

    feat: cartesian to polar test

--- backend/src/core/plane_conversions/cartesian_to_polar.rs
+++ backend/src/core/plane_conversions/cartesian_to_polar.rs
@@ -14,22 +14,58 @@ mod test_cartesian_point {
     use super::*;
 
     #[test]
-    fn expect_to_transform_cartesian_to_polar() {
+    fn expect_to_transform_cartesian_to_polar_on_the_four_axis() {
         assert_eq!(
-            cartesian_to_polar(CartesianPoint { x: 4.0, y: 7.0 }),
-            PolarPoint { x: 6.0, y: 4.0 }
+            cartesian_to_polar(CartesianPoint { x: 1.0, y: 0.0 }),
+            PolarPoint { r: 1.0, a: 0.0 }
         );
         assert_eq!(
-            cartesian_to_polar(CartesianPoint { x: -1.0, y: 3.0 }),
-            PolarPoint { x: 1.0, y: 0.0 }
+            cartesian_to_polar(CartesianPoint { x: 0.0, y: 1.0 }),
+            PolarPoint { r: 1.0, a: 90.0 }
         );
         assert_eq!(
-            cartesian_to_polar(CartesianPoint { x: 4.0, y: 7.0 }),
-            PolarPoint { x: 4.0, y: 7.0 }
+            cartesian_to_polar(CartesianPoint { x: -1.0, y: 0.0 }),
+            PolarPoint { r: 1.0, a: 180.0 }
         );
         assert_eq!(
-            cartesian_to_polar(CartesianPoint { x: -1.0, y: 3.0 }),
-            PolarPoint { x: -1.0, y: 3.0 }
+            cartesian_to_polar(CartesianPoint { x: 0.0, y: -1.0 }),
+            PolarPoint { r: 1.0, a: 270.0 }
+        );
+        assert_eq!(
+            cartesian_to_polar(CartesianPoint { x: 1.0, y: 0.0 }),
+            PolarPoint { r: 1.0, a: 360.0 }
+        );
+    }
+
+    #[test]
+    fn expect_to_transform_cartesian_to_polar_inbetween_the_four_axis() {
+        assert_eq!(
+            cartesian_to_polar(CartesianPoint { x: 0.7071067812, y: 0.7071067812 }),
+            PolarPoint { r: 1.0, a: 45.0 }
+        );
+        assert_eq!(
+            cartesian_to_polar(CartesianPoint { x: -0.7071067812, y: 0.7071067812 }),
+            PolarPoint { r: 1.0, a: 135.0 }
+        );
+        assert_eq!(
+            cartesian_to_polar( CartesianPoint { x: -0.7071067812, y: -0.7071067812 }),
+            PolarPoint { r: 1.0, a: 225.0 }
+        );
+        assert_eq!(
+            cartesian_to_polar(CartesianPoint { x: 0.7071067812, y: -0.7071067812 }),
+            PolarPoint { r: 1.0, a: 315.0 }
+        );
+        assert_eq!(
+            cartesian_to_polar(CartesianPoint { x: 0.7071067812, y: 0.7071067812 }),
+            PolarPoint { r: 1.0, a: 405.0 }
+        );
+    }
+
+    #[test]
+    fn expect_to_transform_cartesian_to_polar_at_the_starting_point() {
+        assert_eq!(
+            cartesian_to_polar(CartesianPoint { x: 0.0, y: 0.0 }),
+            PolarPoint { r: 0.0, a: 0.0 }
         );
     }
 }
