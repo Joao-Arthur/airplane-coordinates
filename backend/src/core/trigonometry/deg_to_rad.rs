e553e3e0d2e4dabedbc84e22fda35c4e9631cc55
joao-arthur <joao.lothamer@gmail.com>
2022-10-23 11:18:14 -0300

    feat: deg to rad new api

--- backend/src/core/trigonometry/deg_to_rad.rs
+++ backend/src/core/trigonometry/deg_to_rad.rs
@@ -1,7 +1,14 @@
-use std::f64::consts::PI;
+use crate::core::precise_decimal::PreciseDecimal;
 
 pub fn deg_to_rad(value: f64) -> f64 {
-    return value * PI / 180.0;
+    return value * std::f64::consts::PI / 180.0;
+}
+
+pub fn new_deg_to_rad(value: PreciseDecimal) -> PreciseDecimal {
+    let half_circle = PreciseDecimal::from_int(180);
+    let pi = PreciseDecimal::from_str(super::consts::PI);
+
+    return value * pi / half_circle;
 }
 
 #[cfg(test)]
@@ -10,14 +17,41 @@ mod test_deg_to_rad {
 
     #[test]
     fn transform_deg_to_rad() {
-        assert_eq!(deg_to_rad(0.0), 0.0);
-        assert_eq!(deg_to_rad(45.0), 0.7853981633974483);
-        assert_eq!(deg_to_rad(90.0), 1.5707963267948966);
-        assert_eq!(deg_to_rad(135.0), 2.356194490192345);
-        assert_eq!(deg_to_rad(180.0), 3.141592653589793);
-        assert_eq!(deg_to_rad(225.0), 3.9269908169872414);
-        assert_eq!(deg_to_rad(270.0), 4.71238898038469);
-        assert_eq!(deg_to_rad(315.0), 5.497787143782138);
-        assert_eq!(deg_to_rad(360.0), 6.283185307179586);
+        assert_eq!(
+            new_deg_to_rad(PreciseDecimal::from_int(0)),
+            PreciseDecimal::from_str("0")
+        );
+        assert_eq!(
+            new_deg_to_rad(PreciseDecimal::from_int(45)),
+            PreciseDecimal::from_str("0.7853981633974483096156608458")
+        );
+        assert_eq!(
+            new_deg_to_rad(PreciseDecimal::from_int(90)),
+            PreciseDecimal::from_str("1.5707963267948966192313216917")
+        );
+        assert_eq!(
+            new_deg_to_rad(PreciseDecimal::from_int(135)),
+            PreciseDecimal::from_str("2.3561944901923449288469825375")
+        );
+        assert_eq!(
+            new_deg_to_rad(PreciseDecimal::from_int(180)),
+            PreciseDecimal::from_str("3.1415926535897932384626433833")
+        );
+        assert_eq!(
+            new_deg_to_rad(PreciseDecimal::from_int(225)),
+            PreciseDecimal::from_str("3.9269908169872415480783042291")
+        );
+        assert_eq!(
+            new_deg_to_rad(PreciseDecimal::from_int(270)),
+            PreciseDecimal::from_str("4.712388980384689857693965075")
+        );
+        assert_eq!(
+            new_deg_to_rad(PreciseDecimal::from_int(315)),
+            PreciseDecimal::from_str("5.4977871437821381673096259206")
+        );
+        assert_eq!(
+            new_deg_to_rad(PreciseDecimal::from_int(360)),
+            PreciseDecimal::from_str("6.2831853071795864769252867667")
+        );
     }
 }
--- backend/src/core/trigonometry/mod.rs
+++ backend/src/core/trigonometry/mod.rs
@@ -1,2 +1,3 @@
+pub mod consts;
 pub mod deg_to_rad;
 pub mod normalize_angle;
