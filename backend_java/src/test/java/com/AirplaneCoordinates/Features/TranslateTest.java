0d59c0821fc85f44df3aa831816f211bd54b8ca8
joao-arthur <joao.lothamer@gmail.com>
2022-10-26 23:22:01 -0300

    feat: translate back and forth test

--- backend_java/src/test/java/com/AirplaneCoordinates/Features/TranslateTest.java
+++ backend_java/src/test/java/com/AirplaneCoordinates/Features/TranslateTest.java
@@ -25,7 +25,7 @@ public final class TranslateTest {
             ).value,
             PlanePoint.from(
                 Plane.POLAR,
-                "2.4142135623730950488016887242",
+                "2.414213562373095048801688724209698078569671875376948073176679737990732478462107038850387534327641572",
                 "45"
             ).value
         );
@@ -39,6 +39,54 @@ public final class TranslateTest {
                 CartesianPoint.from(-2, 2)
             ).value,
             PlanePoint.from(Plane.CARTESIAN, 3, 1).value
+        );
+    }
+
+    @Test
+    public final void backAndForth1() {
+        final var pointAfterMove = Translate.execute(
+            PlanePoint.from(Plane.CARTESIAN, 5, -1),
+            CartesianPoint.from("0.1", "0.1")
+        );
+        final var pointAfterMoveBack = Translate.execute(
+            pointAfterMove,
+            CartesianPoint.from("-0.1", "-0.1")
+        );
+        assertEquals(
+            pointAfterMoveBack.value,
+            PlanePoint.from(Plane.CARTESIAN, 5, -1).value
+        );
+    }
+
+    @Test
+    public final void backAndForth2() {
+        final var pointAfterMove = Translate.execute(
+            PlanePoint.from(Plane.CARTESIAN, "2.22", "3.33"),
+            CartesianPoint.from("5.55", "-5.55")
+        );
+        final var pointAfterMoveBack = Translate.execute(
+            pointAfterMove,
+            CartesianPoint.from("-5.55", "5.55")
+        );
+        assertEquals(
+            pointAfterMoveBack.value,
+            PlanePoint.from(Plane.CARTESIAN, "2.22", "3.33").value
+        );
+    }
+
+    @Test
+    public final void backAndForth3() {
+        final var pointAfterMove = Translate.execute(
+            PlanePoint.from(Plane.CARTESIAN, "5", "5"),
+            CartesianPoint.from("0.5", "0.5")
+        );
+        final var pointAfterMoveBack = Translate.execute(
+            pointAfterMove,
+            CartesianPoint.from("-0.5", "-0.5")
+        );
+        assertEquals(
+            pointAfterMoveBack.value,
+            PlanePoint.from(Plane.CARTESIAN, "5", "5").value
         );
     }
 }
