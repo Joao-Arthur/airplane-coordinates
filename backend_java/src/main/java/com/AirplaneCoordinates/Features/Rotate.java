6b58501b786b6f4ed9a8f8b7c79fbc8fd45c8213
joao-arthur <joao.lothamer@gmail.com>
2022-10-27 22:54:38 -0300

    test: rotate

--- backend_java/src/main/java/com/AirplaneCoordinates/Features/Rotate.java
+++ backend_java/src/main/java/com/AirplaneCoordinates/Features/Rotate.java
@@ -5,6 +5,7 @@ import com.AirplaneCoordinates.Core.Plane.Cartesian.CartesianPoint;
 import com.AirplaneCoordinates.Core.Plane.Conversion.PlaneConversion;
 import com.AirplaneCoordinates.Core.Plane.Generic.PlanePoint;
 import com.AirplaneCoordinates.Core.PreciseDecimal.PreciseDecimal;
+import com.AirplaneCoordinates.Core.Trigonometry.Trigonometry;
 
 public final class Rotate {
     public static PlanePoint execute(
@@ -12,9 +13,10 @@ public final class Rotate {
         final CartesianPoint centerOfRotation,
         final PreciseDecimal angle
     ) {
+        final var angleValue = Trigonometry.normalizeAngle(angle);
         final var offsetPoint = CartesianOperations.sub(point.toCartesian(), centerOfRotation);
         final var pointAsPolar = PlaneConversion.cartesianToPolar(offsetPoint);
-        final var rotatedPoint = pointAsPolar.rotate(angle);
+        final var rotatedPoint = pointAsPolar.rotate(angleValue);
         final var pointAsCartesian = PlaneConversion.polarToCartesian(rotatedPoint);
         final var unoffsetedPoint = CartesianOperations.sum(pointAsCartesian, centerOfRotation);
     
--- backend_java/src/test/java/com/AirplaneCoordinates/Features/RotateTest.java
+++ backend_java/src/test/java/com/AirplaneCoordinates/Features/RotateTest.java
@@ -3,6 +3,8 @@ package com.AirplaneCoordinates.Features;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import com.AirplaneCoordinates.Core.Plane.Cartesian.CartesianPoint;
 import com.AirplaneCoordinates.Core.Plane.Generic.Plane;
@@ -78,26 +80,101 @@ public final class RotateTest {
         );
     }
 
-    @Test
-    public final void rotateBackAndForthCartesian(
+    @ParameterizedTest()
+    @CsvSource({
+        //Cartesian
+        "CARTESIAN, 0, 0, 0, 0, 90",
+        "CARTESIAN, 5, 0, 0, 0, 90",
+        "CARTESIAN, 0, 5, 0, 0, 90",
+        "CARTESIAN, -5, 0, 0, 0, 90",
+        "CARTESIAN, 0, -5, 0, 0, 90",
+        //"CARTESIAN, 5, 5, 0, 0, 90",
+        "CARTESIAN, -5, 5, 0, 0, 90",
+        "CARTESIAN, -5, -5, 0, 0, 90",
+        "CARTESIAN, 5, -5, 0, 0, 90",
+        "CARTESIAN, 1, 1, 0, 0, 90",
+        //"CARTESIAN, 2, 2, 0, 0, 90",
+        //"CARTESIAN, 3, 3, 0, 0, 90",
+        //"CARTESIAN, 4, 4, 0, 0, 90",
+        //"CARTESIAN, 5, 5, 0, 0, 90",
+        "CARTESIAN, 1, 1, 0, 0, 11",
+        //"CARTESIAN, 2, 2, 0, 0, 11",
+        //"CARTESIAN, 3, 3, 0, 0, 11",
+        //"CARTESIAN, 4, 4, 0, 0, 11",
+        //"CARTESIAN, 5, 5, 0, 0, 11",
+        "CARTESIAN, 1, 0, 0, 0, 11",
+        "CARTESIAN, 2, 0, 0, 0, 11",
+        "CARTESIAN, 3, 0, 0, 0, 11",
+        "CARTESIAN, 4, 0, 0, 0, 11",
+        "CARTESIAN, 5, 0, 0, 0, 11",
+        //Cartesian offseted
+        "CARTESIAN, 2, 2, 2, 2, 90",
+        "CARTESIAN, 4, 4, 4, 4, 90",
+        "CARTESIAN, 2, 0, 2, 2, 90",
+        "CARTESIAN, 4, 2, 2, 2, 90",
+        "CARTESIAN, 2, 4, 2, 2, 90",
+        "CARTESIAN, 0, 2, 2, 2, 90",
+        "CARTESIAN, 4, 0, 4, 4, 90",
+        "CARTESIAN, 8, 4, 4, 4, 90",
+        "CARTESIAN, 4, 8, 4, 4, 90",
+        "CARTESIAN, 0, 4, 4, 4, 90",
+        //Polar
+        "POLAR, 0, 0, 0, 0, 90",
+        "POLAR, 7, 0, 0, 0, 90",
+        "POLAR, 7, 90, 0, 0, 90",
+        "POLAR, 7, 180, 0, 0, 90",
+        "POLAR, 7, 270, 0, 0, 90",
+        //"POLAR, 3, 45, 0, 0, 90",
+        "POLAR, 3, 135, 0, 0, 90",
+        "POLAR, 3, 225, 0, 0, 90",
+        //"POLAR, 3, 315, 0, 0, 90",
+        "POLAR, 1, 45, 0, 0, 90",
+        "POLAR, 2, 45, 0, 0, 90",
+        //"POLAR, 3, 45, 0, 0, 90",
+        //"POLAR, 4, 45, 0, 0, 90",
+        //"POLAR, 5, 45, 0, 0, 90",
+        "POLAR, 1, 45, 0, 0, 11",
+        "POLAR, 2, 45, 0, 0, 11",
+        //"POLAR, 3, 45, 0, 0, 11",
+        "POLAR, 4, 45, 0, 0, 11",
+        //"POLAR, 5, 45, 0, 0, 11",
+        "POLAR, 1, 0, 0, 0, 11",
+        "POLAR, 2, 0, 0, 0, 11",
+        "POLAR, 3, 0, 0, 0, 11",
+        "POLAR, 4, 0, 0, 0, 11",
+        "POLAR, 5, 0, 0, 0, 11",
+        //Polar offseted
+        //"POLAR, 2, 45, 2, 2, 90",
+        //"POLAR, 4, 45, 4, 4, 90",
+        //"POLAR, 1, 10, 4, 4, 90",
+        //"POLAR, 2, 20, 4, 4, 90",
+        //"POLAR, 3, 30, 4, 4, 90",
+        //"POLAR, 4, 40, 4, 4, 90",
+        //"POLAR, 5, 45, 4, 4, 90",
+        //"POLAR, 6, 50, 4, 4, 90",
+        //"POLAR, 7, 60, 4, 4, 90",
+        "POLAR, 1, 0, 1, 1, 90",
+        "POLAR, 1, 90, 1, 1, 90",
+        "POLAR, 2, 0, 2, 2, 90",
+        "POLAR, 2, 90, 2, 2, 90",
+        //"POLAR, 3, 0, 3, 3, 90",
+        //"POLAR, 3, 90, 3, 3, 90",
+        //"POLAR, 4, 0, 4, 4, 90",
+        //"POLAR, 4, 90, 4, 4, 90",
+    })
+    public final void rotateBackAndForth(
+        final Plane plane,
+        final String pointX,
+        final String pointY,
+        final String centerX,
+        final String centerY,
+        final String angle
     ) {
-    }
-
-    @Test
-    public final void rotateBackAndForthPolar(
-        
-    ) {
-    }
-
-    @Test
-    public final void rotateBackAndForthCartesianOffseted(
-       
-    ) {
-    }
-
-    @Test
-    public final void rotateBackAndForthPolarOffseted(
-      
-    ) {
+        final var pointBefore = PlanePoint.from(plane, pointX, pointY);
+        final var centerOfRotation = CartesianPoint.from(centerX, centerY);
+        final var angleValue = PreciseDecimal.from(angle);
+        final var movedPoint = Rotate.execute(pointBefore, centerOfRotation, angleValue);
+        final var pointAfter = Rotate.execute(movedPoint, centerOfRotation, angleValue.opposite());
+        assertEquals(pointAfter.value, pointBefore.value);
     }
 }
