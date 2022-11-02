447c560f81bfb8ae53aa0ef0bd94c2e6fbe32ed5
joao-arthur <joao.lothamer@gmail.com>
2022-11-01 22:29:21 -0300

    test: linear function from point

--- backend/src/test/java/com/AirplaneCoordinates/Core/LinearFunction/LinearFunctionTest.java
+++ backend/src/test/java/com/AirplaneCoordinates/Core/LinearFunction/LinearFunctionTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import com.AirplaneCoordinates.Core.Plane.Cartesian.CartesianPoint;
 import com.AirplaneCoordinates.Core.PreciseDecimal.PreciseDecimal;
 
 public final class LinearFunctionTest {
@@ -15,6 +16,87 @@ public final class LinearFunctionTest {
                 PreciseDecimal.from("7.26")
             ).toString(),
             "y = -5x +7.26"
+        );
+    }
+
+    @Test
+    public final void from() {
+        assertEquals(
+            LinearFunction.from(
+                CartesianPoint.from(1, 1),
+                PreciseDecimal.from(45)
+            ),
+            LinearFunction.from(1,0).toString()
+        );
+        assertEquals(
+            LinearFunction.from(
+                CartesianPoint.from(0, 0),
+                PreciseDecimal.from(45)
+            ),
+            LinearFunction.from(1,0).toString()
+        );
+        assertEquals(
+            LinearFunction.from(
+                CartesianPoint.from(-1, -1),
+                PreciseDecimal.from(45)
+            ),
+            LinearFunction.from(1,0).toString()
+        );
+        assertEquals(
+            LinearFunction.from(
+                CartesianPoint.from(-1, 1),
+                PreciseDecimal.from(135)
+            ),
+            LinearFunction.from(-1,0).toString()
+        );
+        assertEquals(
+            LinearFunction.from(
+                CartesianPoint.from(0, 0),
+                PreciseDecimal.from(135)
+            ),
+            LinearFunction.from(-1,0).toString()
+        );
+        assertEquals(
+            LinearFunction.from(
+                CartesianPoint.from(1, -1),
+                PreciseDecimal.from(135)
+            ),
+            LinearFunction.from(-1,0).toString()
+        );
+        assertEquals(
+            LinearFunction.from(
+                CartesianPoint.from(-1, 1),
+                PreciseDecimal.from(0)
+            ),
+            LinearFunction.from(0, 1).toString()
+        );
+        assertEquals(
+            LinearFunction.from(
+                CartesianPoint.from(0, 0),
+                PreciseDecimal.from(0)
+            ),
+            LinearFunction.from(0, 0).toString()
+        );
+        assertEquals(
+            LinearFunction.from(
+                CartesianPoint.from(1, -1),
+                PreciseDecimal.from(0)
+            ),
+            LinearFunction.from(0, -1).toString()
+        );
+        assertEquals(
+            LinearFunction.from(
+                CartesianPoint.from(2, 0),
+                PreciseDecimal.from(45)
+            ),
+            LinearFunction.from(1, -2).toString()
+        );
+        assertEquals(
+            LinearFunction.from(
+                CartesianPoint.from(2, 4),
+                PreciseDecimal.from(45)
+            ),
+            LinearFunction.from(1, 2).toString()
         );
     }
 }
