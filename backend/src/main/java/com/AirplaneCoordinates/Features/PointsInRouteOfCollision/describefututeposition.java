54943c82343f6968dfe31ca7697e023fe05e66b4
joao-arthur <joao.lothamer@gmail.com>
2022-11-11 20:17:53 -0300

    chore: setup possible new api for collision

---
+++ backend/src/main/java/com/AirplaneCoordinates/Features/PointsInRouteOfCollision/describefututeposition.java
@@ -0,0 +1,34 @@
+package com.AirplaneCoordinates.Features.PointsInRouteOfCollision;
+
+import com.AirplaneCoordinates.Core.Plane.Cartesian.CartesianPoint;
+
+public class describefututeposition {
+    public void execute() {
+        linearPointXA = linearpoint;
+        linearPointXb = linearpoint;
+        linearPointya = linearpoint;
+        linearPointyb = linearpoint;
+
+        collisionInX;
+        collisioninY;
+
+        if(collisionInX.x == collisioninY.x) {
+            //colisão vai acontecer nos dois eixos ao mesmo tempo, ou seja, tem uma colisão
+        } else {
+            // colisão não vai acontecer ao mesmo tempo, não tem uma colisão.
+
+            //será que nesses casos, vai descrever corretamente
+                - same function?
+                - differeent function?
+                - same position?
+                - parallel lines?
+                - 
+        }
+
+
+
+        final var collisionPointInTheFuture = CartesianPoint(collisionInX, collisioniny);
+
+        
+    }
+}
